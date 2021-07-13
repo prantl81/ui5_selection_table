b61cf35502d88629a2ca955e3576da8a9c9e64c5
prantl81 <33022108+prantl81@users.noreply.github.com>
2021-07-13 13:23:27 +0200

    Update selectiontable.js

--- selectiontable.js
+++ selectiontable.js
@@ -104,6 +104,10 @@
                 var context = oTable.getContextByIndex(oTable.getSelectedIndex());
                 var value = context.getProperty("Name");
 
+                path = event.getSource().getBindingContext().getPath();
+                let oModel = oTable.getModel();
+                var obj = oModel.getProperty(path);
+
                 this.dispatchEvent(new Event("onSelectionChange", {
                     detail: {
                         properties: {
